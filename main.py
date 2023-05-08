4105a3185281a7de7bcd5423c9a9a3d05a8148f1
Manish123654 <mayurathod.111.mr@gmail.com>
2023-05-08 18:18:14 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,127 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+# f print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    # print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+# if __name__ == '__main__':
+#     print_hi('PyCharm')
+#
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+from selenium import webdriver
+# from selenium.webdriver.common.by import By
+# import time
+
+
+# browser = webdriver.Chrome("C:\\Users\\yusuf\\Downloads\\chromedriver_win32\\chromedriver")
+# obj=              a.Myclass7(20)
+
+# username = "yusuft"
+# password = "Password"
+#
+# url = 'https://www.facebook.com/'
+# browser.get(url)
+# time.sleep(5)
+#
+# browser.find_element(By.NAME, "email").send_keys(username)
+# browser.find_element(By.NAME, "pass").send_keys(password)
+# time.sleep(10)
+# browser.find_element(By.NAME,"login").click()
+# browser.maximize_window()
+# time.sleep(15)
+# browser.minimize_window()
+# time.sleep(5)
+#
+# browser.close()
+#
+
+# import time
+# from selenium import webdriver
+# from selenium.webdriver.common.by import By
+#
+# browser = webdriver.Chrome()
+# time.sleep(5)
+# url = 'https://www.facebook.com/'
+# browser.get(url)
+#
+# browser.find_element(By.NAME, "email").send_keys("yusitamboli")
+# browser.find_element(By.NAME, "pass").send_keys("yusuf123@")
+# time.sleep(5)
+# browser.find_element(By.NAME,"login").click()
+#
+# browser.maximize_window()
+# time.sleep(5)
+# browser.close()
+
+
+
+# from selenium import webdriver
+# from selenium.webdriver.common.by import By
+# import time
+#
+# browser = webdriver.Firefox()
+# username = 9090909090
+# password = "Password"
+# time.sleep(5)
+# url = 'https://www.facebook.com/'
+# browser.get(url)
+#
+# browser.find_element(By.ID, "email").send_keys(username)
+# browser.find_element(By.ID, "pass").send_keys(password)
+# time.sleep(5)
+# browser.find_element(By.NAME,"login").click()
+#
+# browser.maximize_window()
+# time.sleep(5)
+# browser.maximize_window()
+# time.sleep(5)
+#
+# browser.close()
+
+#
+# import time
+# from selenium import webdriver
+# from selenium.webdriver.common.by import By
+# options = webdriver.ChromeOptions()
+# options.add_experimental_option("detach",True)
+# browser = webdriver.Chrome(options=options)
+# username = "yusitamboli"
+# password = "Yusi123@"
+# time.sleep(5)
+# url = 'https://www.facebook.com/'
+# browser.get(url)
+#
+# browser.find_element(By.NAME, "email").send_keys(username)
+# browser.find_element(By.NAME, "pass").send_keys(password)
+# browser.find_element(By.NAME,"login").click()
+#
+# time.sleep(5)
+# browser.find_element(By.ID,"u_0_5_1V")
+#
+# browser.maximize_window()
+# browser.set_window_size(500,500)
+# print(browser.set_window_position(200,200))
+#
+# time.sleep(5)
+#
+# from selenium import webdriver
+# import time
+# driver = webdriver.Chrome()
+# driver.get("https://www.instagram.com/")
+#
+# driver.implicitly_wait(30)
+# driver.find_element("username").send_keys("yusitamboli")
+# time.sleep(10)
+#
+# driver.find_element("password").send_keys("yusi123@")
+#
+# time.sleep(10)
+# driver.find_element("Log in").click()
+#
+#
---
+++ practice.py
@@ -0,0 +1,374 @@
+'''
+print("Welcome to Credence Automation testing Lecture with Yusuf Sir And Tushar Sir")
+print("Hello All")
+print("Hello Guys"
+"How r u")
+print("Yusuf\n"
+"Tushar")
+print("Yusuf\n"  "Tushar")
+print("""Yusuf
+Tushar""")
+print("Hello Guys");print("How r u")
+'''
+
+#print("Want to display this line") # This syntax will be display "Want to display this line"
+
+#Below script is written for login page
+""" 
+print("Welcome to Credence Automation testing Lecture with Yusuf Sir And Tushar Sir")
+print("Hello All")
+print("Hello Guys"
+"How r u")
+
+
+"""
+
+# print("Welcome to Credence Automation testing Lecture with Yusuf Sir And Tushar Sir")
+# print("Hello All")
+# print("Hello Guys"
+# "How r u")--- contol+/
+# #
+# print("Hello All")
+# print('Hello All')
+# print('We need a chaperone');
+# print("'Hello All', 'Good Morning'")
+
+# if(5>3):
+# print(" first value is bigger than seccond")
+
+
+# import keyword
+# print(keyword.kwlist)
+
+thought= 'negative'
+if thought == "negative":
+    print("No 15lac offer")
+    print("No good life ")
+    print("No financial Security")
+
+# Identifiers
+# a, b, add, name, number these are identofiers which can be used to give names variables,
+# functions classes
+
+
+# # Variables
+# Variables (box) is a storage area which holds the data. it is defined by using identifier.
+# every variable we can call them as identifiers-- box1 box2
+# but not possible with vice versa.
+# not becz idenfires can be functions classes or anything.
+
+
+# number=10
+# print(number)
+#
+# a=20
+# site_name = 'cred.pro'
+# print(site_name)
+# print(a)
+#
+# number=100
+# print(number)
+#
+# a=200
+# site_name = 'cred.com'
+# print(site_name)
+# print(a)
+#
+# number=10
+# a=20
+# site_name = 'cred.pro'
+#
+# number=100
+# a=200
+# a=float(a)
+# print (a)
+# site_name = 'cred.com'
+# print(number)
+# print(site_name)
+# print(a)
+
+# site_name = 'cred.com'
+# print(site_name)
+#
+# # assigning a new value to site_name
+# site_name = 'apple.com'
+#
+# print(site_name)
+
+# number,a,site_name=1000,2000,'orange.com'
+# print(number)
+# print(a)
+# print(site_name)
+
+# name1=name2='Yusuf'
+# print(name1,name2)
+
+# a,b='red','blue'
+# print(a,b)
+# a,b=b,a
+# print(a,b)
+
+# x,y='Yusuf','Tushar'
+# a='Amit'
+# b='Sameer'
+#
+# print(a,b)
+# print(x,y)
+#
+# x,y=y,x
+# a,b=b,a
+#
+# print(a,b)
+# print(x,y)
+
+
+# x,y='Yusuf','Tushar'
+# a='Amit'
+# b='Sameer'
+#
+# print(a,b)
+# print(x,y)
+#
+# x,y=a,b
+#
+# print(a,b)
+
+# a=10
+# del a
+# print(a)
+
+# Global variable examole
+# a=100
+# b=200
+# print(a+b)
+
+
+# local Variable examples
+# def add():
+#     a = 10
+#     b = 20
+#     print(a + b)
+
+# add()
+
+# a=10
+# name='yusuf'
+# c=10.5
+# d=100+10j
+# type(a)
+# print(type(a))
+# print(type(name))
+# print(type(c))
+# print(type(d))
+#
+#
+# print(isinstance(d,complex))
+# print(isinstance(a,complex))
+# print(isinstance(name,str))
+# print(isinstance(c,str))
+# print(isinstance(c,float))
+
+# a=10
+# type(a)
+# id(a)
+# print(type(a))
+# print(id(a))
+
+# a=10
+# print(id (a))
+# print(type (a))
+
+# a,b,c= 10,20,30
+# print(id (a))
+# print(id (b))
+# print(id (c))
+
+# a,b,c= 10,10,20
+# print(id (a))
+# print(id (b))
+# print(id (c))
+# print(a)
+# print(b)
+# print(c)
+# # 3 location ids
+
+# a,b,c= 10,10,20
+# print(id (a))
+# print(id (b))
+# print(id (c))
+#
+# a,b,c= 10,10,10
+# print(id (a))
+# print(id (b))
+# print(id (c))
+# a=10
+# print(id (a))
+# a=20
+# print(id (a))
+
+# a=b=c=100
+# print(id(a),id(b),id(c))
+# a=10
+# name='yusuf'
+# c=10.5
+# d=100+10j
+
+# 10- int literal
+# Yusuf- string literal
+# 10.5 – float literal
+# 100+10j- complex literal
+
+# a= 'Yusuf'
+# print(type(a))
+#
+# b=20
+# print(type(b))
+# e=100j
+# print(type(e))
+
+# a=10
+# b=10.0
+# print(id(a),id(b))
+
+# open("geek.txt","r")
+# print(open("geek.txt","r"))
+# print(open("geek.txt","r").read())
+#
+# f1 = open("geek.txt","r")
+# print(f1)
+# print(f1.read())
+# f1.close()
+
+
+
+# f1 = open("geek.txt","w")
+# f1.write("This is Tushar/n")
+# f1.close()
+
+
+# f1 = open("geek.txt","a")
+# f1.write("This is Tushar/n")
+# f1.write("This is Amit\n")
+# f1.write("This is Sameer\n")
+
+
+# f1 = open("credence.txt","x")
+#
+# f1.close()
+
+# f = open("C:\\Users\yusuf\OneDrive\Desktop\File1.txt", "r")
+#
+# print(f.read())
+
+# f = open("geek.txt", "r")
+# print(f.read(5))
+
+# f = open("C:\\Users\yusuf\OneDrive\Desktop\File1.txt", "r")
+# print(f.read())
+
+# f = open("C:\\Users\yusuf\OneDrive\Desktop\File1.txt", "a")
+# f.write("\nThis is Automation Class")
+# f.close()
+#
+# f = open("C:\\Users\yusuf\OneDrive\Desktop\File1.txt", "r")
+# print(f.read())
+
+# file1=open("C:\\Users\yusuf\OneDrive\Desktop\File1.txt","w")
+# file1.write("\nsomething")
+# file1.close()
+
+# f = open("geek.txt", "r")
+# print(f.read())
+# print(f.readline())
+# print(f.readline())
+# print(f.readlines())
+# print(f.readable())
+# print(f.tell())
+# print(f.seek(10))
+# print(f.read(8))
+# f.close()
+
+
+# f = open("geek.txt", "r")
+# for x in f:
+#   print(x)
+# f.close()
+
+# f = open("C:\\Users\yusuf\OneDrive\Desktop\File1.txt", "r")
+# print(f.readlines())
+# f.close()
+
+# f = open("geek.txt", "a")
+# f.write("Now the file has more content!")
+# f.close()
+#
+# #open and read the file after the appending:
+# f = open("geek.txt", "r")
+# print(f.read())
+#
+# f = open("geek.txt", "w")
+# f.write("Woops! I have deleted the content!")
+# f.close()
+#
+# #open and read the file after the overwriting:
+# f = open("geek.txt", "r")
+# print(f.read())
+#
+
+# fileptr = open("file2.txt", "w")
+
+# # appending the content to the file
+# fileptr.write('''Python is the modern day language. It makes things so simple.
+# It is the fastest-growing programing language''')
+#
+# # closing the opened the file
+# fileptr.close()
+#
+# with open("filex.txt",'x') as f:
+#     f.write("Yusuf")
+# f.close()
+#
+# with open("file.txt",'a') as f:
+#     f.write("\nTushar")
+#     f.close()
+#
+# with open("file.txt",'r') as f:
+#     print(f.read())
+
+# import os
+# if os.path.exists("file.txt"):
+#   os.remove("file.txt")
+# else:
+#   print("The file does not exist")
+
+
+# import os
+# os.remove("filex.txt")
+
+# import os
+# # rename file.txt to filerename.txt
+# os.rename("file.txt", "filerename.txt")
+
+
+# import os
+# if os.path.exists("filerename.txt"):
+#   os.remove("filerename.txt")
+# else:
+#   print("The file does not exist")
+# import os
+# os.remove("filerename.txt")
+#
+
+
+# import os
+#
+# try:
+#     os.remove("filerename.txt")
+#     print('The file is removed.')
+# except FileNotFoundError:
+
+
+# f1=open("fileappend.txt","r+")
+# print(f1.read())
+# f.close()
+
